a8e642ffb81c7c85f4c4afc326b6b848c927a49a
SandraKay77 <ladivina77@gmail.com>
2023-02-11 14:26:28 -0800

    base layout

--- mysite.js
+++ mysite.js
@@ -1,3 +1,46 @@
 $(
+    function(){
+        const page_names =  ["Index", "About_Me", "Art_Projects", "CV"];
+        // const body = 
+
+        function createPage(){
+            navBar();
+            
+        }
+
+        function navBar(){
+            pglayout();
+            var navList = $('<div id="nav"> <ul id="navlist"> ' + '</ul> </div>');
+            $('body').append(navList);
+        let page = "";
+            for (let x of page_names){
+            page = $('<li> <a href=' + x + '.html>' + x + '</a> </li>');
+            $('#navlist').append(page);
+ 
+        }
+
+        function pglayout(){
+            var header = $('<div id="header" class="center"> <h1>Sandra Schreffler</h1> </div>');
+            var mainDiv = $('<div id="content" class="center"></div>');
+
+            $('body').append(header).append(mainDiv);
+        }
+
+
+     
+    }
+
     
-)
+
+
+
+
+
+
+    return function(evt){
+        createPage();
+
+    }
+
+    }()
+);
