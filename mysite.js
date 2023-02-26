241649200ddf48b34d03a701b7e8fcd443671da2
SandraKay77 <ladivina77@gmail.com>
2023-02-25 23:10:59 -0800

    cv adjustments

--- mysite.js
+++ mysite.js
@@ -1,123 +1,93 @@
 $(
-    function(){
-        const PAGE_NAMES =  ["Index", "About_Me", "Art_Projects", "CV"];
-        
+    function () {
+        const PAGE_NAMES = ["Index", "About_Me", "Art_Projects", "CV"];
+
         var WINDOW_WIDTH = $(window).width();
         var WINDOW_HEIGHT = $(window).height();
-        
 
-        function createPage(){
+
+        function createPage() {
             navBar();
-            myObjectives();
-            
+            // myObjectives();
+
         }
 
-        function navBar(){
+        function navBar() {
             pglayout();
 
             var navButton = "<span class='label'>Menu</span>";
             var burgerButton = $("<span/>", {
                 class: 'burgerbutton fa-solid fa-bars',
             });
-
             var navList = $("<ul/>", {
                 id: "items-list",
-                class: "items-list" 
+                class: "items-list"
             });
 
-            var page1 = $("<li class='navItem'><i class='iconitem fa-solid fa-house'></i>  Home </li>");
-            var page2 = $("<li class='navItem'> <i class='iconitem fa-solid fa-file-lines'></i> Resume</li>");
-            var page3 = $("<li class='navItem'><i class='iconitem fa-solid fa-paintbrush'></i> Art</li>");
-            var page4 = $("<li class='navItem'> <i class='iconitem fa-regular fa-user'></i>About </li>");
+            var page1 = $("<li class='navItem'><i class='iconitem fa-solid fa-house'></i>  <a href='index.html'> Home </href></li>");
+            var page2 = $("<li class='navItem'> <i class='iconitem fa-solid fa-file-lines'></i> <a href='cv.html'> Resume</href></li>");
+            var page3 = $("<li class='navItem'><i class='iconitem fa-solid fa-paintbrush'></i> <a href='Art_projects.html'> Art</href></li>");
+            var page4 = $("<li class='navItem'> <i class='iconitem fa-regular fa-user'></i><a href='About_me.html'>About </href></li>");
 
 
             $('#dropMenu').append(navButton, burgerButton, navList);
-            navList.append(page1, page2, page3, page4); 
+            navList.append(page1, page2, page3, page4);
 
-          
-            
-  
+
+
+
 
         }
 
-        function dropMenu(){
-            console.log("Hello World")
-            
+        function dropMenu() {
             $('.items-list').toggleClass('open')
-
-
-            // document.addEventListener('click', ($event) =>
-            // $event.target !== $burgerButton &&
-            // $containerItems.classList.remove('open'));
         }
-    
-        function pglayout(){
+
+        function pglayout() {
+            $('body').append(
+            $("<div/>", {
+                id: "navbar",
+                class: "container"
+                // click: function () {
+                //     dropMenu();
+                // }
+            }),
+            
             $("<div/>", {
                 id: "header",
                 class: "center",
-                html: "<h1> Sandra Schreffler </h1>",
-                css: {
-                    "backgroundColor": "var(--light2)",
-                    "width": "50%"
-            }   
-            }).appendTo('body');
-
-            $("<div/>", {
-                id: "navbar",
-                class: "container",
-                css: {
-                    "margin": "0 auto", 
-                    "max-width": "250px"
-                },
-                click: function(){
-                    dropMenu();
-                }
-                }).appendTo('body');
+                html: "<h1> Sandra Schreffler </h1>"
+            }));
 
             $("<div/>", {
                 class: "dropdown",
-                id: "dropMenu"
-        }
+                id: "dropMenu",
+                click: function () {
+                    dropMenu();
+                }
+            }
             ).appendTo('#navbar')
-                
-            // $('body').append(header, navBar);
-
-            
         }
 
 
         // responsive updating the window size
-        function resize(){
+        function resize() {
 
         }
-// building Objective section of CV 
-        function myObjectives(){
-            $("<div/>", {
-                    id: "objective",
-                    class: "center"
-                }).css(
-                    {
-                        "backgroundColor": "var(--dark1)",
-                        "font-size": "1em",
-                        "width": "30%",
-                        "float": "right"
-                    }
-                    ).appendTo('#cvPage').html('<p>hello world</p>');
-                
-            }
-    
-    
+      
 
 
 
 
 
 
-    return function(evt){
-        $(window).resize();
-        createPage();
 
-    }
+
+        return function (evt) {
+            $(window).resize();
+            createPage();
+
+        }
 
     }()
 );
