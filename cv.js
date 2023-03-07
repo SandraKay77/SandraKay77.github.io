cff19b538776cd6780bd7823bdbf948b96ba5423
Sandra Schreffler <56328497+SandraKay77@users.noreply.github.com>
2023-03-06 17:05:09 -0800

    looks like something

--- cv.js
+++ cv.js
@@ -2,79 +2,130 @@ $(
     // Containing function 
     function () {
 
+    const CARET = $(
+        "<span/>", {
+            id: 'myCaret',
+            class: 'fa-solid fa-angle-right'
+         });
+
         // draw the pieces of the page 
         function cvPage() {
             cvLayout();
             skillSets();
+            myObjective();
+            workExperience();
+            myProjects();
 
         }
+
+        // initial box layout
         function cvLayout(){
             $('body').append(
                 $("<div/>", {
                     class: 'cvContainer'
-                }),
-                $("<div/>", {
-                    id: "contact",
-                    class: "center contact",
-                    html: '<h4>Contact:</h4>'
-                }));
-
-            $('.cvContainer').append(
+                })
+                );
+            
+                $('.cvContainer').append(
 
             $("<div/>", {
                 id: "objective",
-                class: "boxes",
-                html: '<h4>Objective</h4>'
+                class: "boxes objective",
             }),
 
             $("<div/>", {
-                class: 'boxes',
+                class: 'boxes skills',
                 id: 'skills',
-                click: function (e) {
-                    openMenu();}
+                css: ({'grid-column-start': 2})
             }),
-
+            
             $("<div/>", {
                 id: "work",
-                class: "boxes",
-                html: '<h4>Experience</h4>'
+                class: "boxes work",
             }),
 
             $("<div/>", {
-                id: "Projects",
-                class: "boxes",
-                html: '<h3>Other Projects</h3>'
+                id: "projects",
+                class: "boxes projects",
+                css: ({'grid-column-start': 2})
             }));
-
+            $('.boxes').append(
+                $('<div/>', {
+                    class: 'subHeader',
+                    click: function (evt) {
+                        openMenu(evt);}
+                }).append(
+                    $('<span/>', {
+                        class: 'label'
+                    }),
+                    CARET
+                ));
         }
-
+        // complete "skills" box
         function skillSets(){
-            var skillBlock = "<span class='label'>Skills</span>";
-            var closeCaret = $("<span/>", {
-                class: 'caret fa-solid fa-angle-right',
-        });
-
-        $('<div/>', {
-            class: 'panel'
-        }
-        ).append(skillBlock, closeCaret).appendTo('#skills');
-
-                var skillList = $('<ul/>', {
+        $('#skills').find('.label').text('Skills');
+        // $('<div/>', {
+        //     class: 'subHeader',
+        //     click: function (evt) {
+        //         openMenu(evt);}
+        // }).append(skillBlock, CARET));
+                let skillList = $('<ul/>', {
                 id: 'skillList',
                 class: 'all-list'
             });
             
-            var skill1 = $('<li class="skillItem">Lorem Ipsum bl    ah blah</li>');
-            skillList.append(skill1);
+            let skill1 = $('<li class="allItem">Analytical Problem Solver</li>');
+            let skill2 = $('<li class="allItem">Programming Languages</li> ');
+            let programList = $('<ul><li>Python</li><li>Java</li> <li>Javascript</li> <li>PHP</li> <li>Bash</li> <li>Powershell</li></ul>');
+            let skill3 = $('<li class="allItem">Network and Data Communications</li>');
+            let networkList = $('<ul><li>Wireshark</li><li>FTP, DNS, DHCP</li> <li>Network Security and Management</li> </ul>');
+            let skill4 = $('<li class="allItem">Security Tools</li>');
+            let secureList = $('<ul><li>Kali Linux</li><li>Metasploit</li> <li>aircrack-ng</li> </ul>');
+            let skill5 = $('<li class="allItem">Operating Systems</li>');
+            let osList = $('<ul><li>Windows</li><li>Linux</li></ul>');
+            skillList.append(skill1, skill2, skill3, skill4, skill5);
 
             $('#skills').append(skillList);
             
-
-
         }
-        function openMenu() {
-            $('.all-list').toggleClass('open');
-            // $('.boxes').toggleClass('panel');
+
+        function myObjective(){
+            $('#objective').find('.label').text('Objective');
+            
+                    let obj = $('<table/>', {
+                    id: 'objective',
+                    class: 'all-list'
+                });
+                
+                let obj1 = '<thead class="allItem">\
+                    <tr><th>Currently seeking a position in Software Development and/or Security where my education and passion will provide value to the employer and the opportunity for further hands-on experience.\
+                    </th></tr>\
+                </thead>';
+                obj.append(obj1);
+    
+                $('#objective').append(obj);
+                
+            }
+
+            function workExperience(){
+                $('#work').find('.label').text('Work Experience');
+
+            }
+
+            
+            function myProjects(){
+                $('#projects').find('.label').text('Extracurricular and Other Projects');
+
+            }
+    
+    
+
+
+        function openMenu(clicked) {
+            myNode = clicked.target;
+            classPanel = $(myNode).next('.all-list');
+            myCaret = $(myNode).find('#myCaret').toggleClass('fa-solid fa-angle-right fa-solid fa-angle-down');
+            classPanel.slideToggle("slow");
         }
 
         //   returnin the opening function 
