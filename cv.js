6cc68328fbfc724bac31ced948d0d27ef61e9ef4
Sandra Schreffler <56328497+SandraKay77@users.noreply.github.com>
2023-03-06 17:05:09 -0800

    cv adjustments

--- cv.js
+++ cv.js
@@ -1,81 +1,89 @@
 $(
     // Containing function 
-    function(){
-        // listing the pages on the site 
-        const page_names =  ["Index", "About_Me", "Art_Projects", "CV"];
-        
-// draw the pieces of the page 
-        function drawPage(){
-            navBar();
-            myObjectives();
-            
-            
+    function () {
+
+        // draw the pieces of the page 
+        function cvPage() {
+            cvLayout();
+            skillSets();
+
         }
-// adding navigation bar 
-        function navBar(){
-            pglayout();
-            var navList = $('<div id="nav"> <ul id="navlist"> ' + '</ul> </div>');
-            $('body').append(navList);
-        let page = "";
-            for (let x of page_names){
-            page = $('<li> <a href=' + x + '.html>' + x + '</a> </li>');
-            $('#navlist').append(page);
-            }
-        }
-// base layout of all pages
-        function pglayout(){
-            var header = $("<div/>", {
-                id: "header",
-                class: "center"
-                
+        function cvLayout(){
+            $('body').append(
+                $("<div/>", {
+                    class: 'cvContainer'
+                }),
+                $("<div/>", {
+                    id: "contact",
+                    class: "center contact",
+                    html: '<h4>Contact:</h4>'
+                }));
 
-            }).css(
-                {
-                    "backgroundColor": "var(--light2)"
-                }
+            $('.cvContainer').append(
 
-            ).html('<h1> Sandra Schreffler </h1>'
-            );
-
-            var mainDiv = $("<div/>", {
-                id: "content",
-            }
-            );
-
-            $('body').append(mainDiv).append(header);
-
-            // pageSize('content');
-        }
-
-        //responsive page size
-        // function pageSize(divID){
-        //     pageDiv = $(divID);
-        //     pageDiv
-        // }
-
-        
-        function myObjectives(){
             $("<div/>", {
-                    id: "objective",
-                    class: "center"
-                }).css(
-                    {
-                        "backgroundColor": "var(--dark1)",
-                        "font-size": "1em",
-                        "width": "30%",
-                        "float": "right"
-                    }
-                    ).appendTo('#content').html('<p>hello world</p>');
-                
-            }
-    
-//   returnin the opening function 
-    return function(evt){
-        drawPage();
-        
+                id: "objective",
+                class: "boxes",
+                html: '<h4>Objective</h4>'
+            }),
 
-    }
+            $("<div/>", {
+                class: 'boxes',
+                id: 'skills',
+                click: function (e) {
+                    openMenu();}
+            }),
 
-    // wrapping up the opening function
-}()
+            $("<div/>", {
+                id: "work",
+                class: "boxes",
+                html: '<h4>Experience</h4>'
+            }),
+
+            $("<div/>", {
+                id: "Projects",
+                class: "boxes",
+                html: '<h3>Other Projects</h3>'
+            }));
+
+        }
+
+        function skillSets(){
+            var skillBlock = "<span class='label'>Skills</span>";
+            var closeCaret = $("<span/>", {
+                class: 'caret fa-solid fa-angle-right',
+        });
+
+        $('<div/>', {
+            class: 'panel'
+        }
+        ).append(skillBlock, closeCaret).appendTo('#skills');
+
+                var skillList = $('<ul/>', {
+                id: 'skillList',
+                class: 'all-list'
+            });
+            
+            var skill1 = $('<li class="skillItem">Lorem Ipsum bl    ah blah</li>');
+            skillList.append(skill1);
+
+            $('#skills').append(skillList);
+            
+
+
+        }
+        function openMenu() {
+            $('.all-list').toggleClass('open');
+            // $('.boxes').toggleClass('panel');
+        }
+
+        //   returnin the opening function 
+        return function (evt) {
+            cvPage();
+
+
+        }
+
+        // wrapping up the opening function
+    }()
 );
