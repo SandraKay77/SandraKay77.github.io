343a2ca09ae701740315d25a06008a47893bb0d5
Sandra Schreffler <56328497+SandraKay77@users.noreply.github.com>
2025-06-16 14:00:52 -0600

    working, pre css alterations

---
+++ contactform.php
@@ -0,0 +1,16 @@
+<html>
+    <body>
+        <script>
+            function submission(){
+                
+            }
+            </script>
+        
+        Thank You for Your Submission, <?php echo
+        
+        $_POST["userName"];
+        
+        ?>.
+
+        </body>
+        </html>
